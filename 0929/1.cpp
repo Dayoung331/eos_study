9b6f1f75324acc56783567d561977db9907bf67d
Dayoung331 <pinga331@hanyang.ac.kr>
2022-09-29 15:59:03 +0900

    09/29

---
+++ 0929/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char **argv){
+    int n = atoi(argv[argc-1]);
+    string s = argv[1];
+    for(int i=0; i<n; i++){
+        cout << s << endl;
+    }
+
+    return 0;
+}
