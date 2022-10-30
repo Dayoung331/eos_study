1f24fdb7d75394de1e66d4d4fef0ad3cfee81d64
Dayoung331 <pinga331@hanyang.ac.kr>
2022-10-30 18:54:37 +0900

    10/30

---
+++ 10/30.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str("There are two needles in this haystack with needles.");
+    string str2("needle");
+    size_t found;
+
+    if((found = str.find(str2)) != string::npos){
+        cout << "firt 'needle found at: " << int(found) << endl;
+    }
+    str.replace(str.find(str2), str2.length(), "preposition");
+    cout << str << endl;
+
+    return 0;
+}
